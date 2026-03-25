465c7be60e2bdc6c5c69ea43eae11b2f6d2540f7
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2026-03-25 20:47:56 +0000

    Header css

--- atv-react/src/components/ContadorHistorico.jsx
+++ atv-react/src/components/ContadorHistorico.jsx
@@ -4,12 +4,12 @@ export default function ContadorHistorico({contador}) {
     const [valor, setValor] = useState(0);
     const [historico, setHistorico] = useState([]);
     
-    const handleIncrementar = () => {
+    const Incrementar = () => {
         setValor(valor + 1);
         setHistorico([...historico, valor + 1]);
     }
 
-    const handleDecrementar = () => {
+    const Decrementar = () => {
         setValor(valor - 1);
         setHistorico([...historico, valor - 1]);
     }
@@ -19,8 +19,8 @@ export default function ContadorHistorico({contador}) {
             <h2 className="title2">Contador</h2>
             <p>Valor atual do contador: {valor}</p>
 
-            <button className="counter" onClick={handleIncrementar} style={{marginTop: '10px'}}>Incrementar</button>
-            <button className="counter" onClick={handleDecrementar} style={{ marginLeft: '10px' }}>Decrementar</button>
+            <button className="counter" onClick={Incrementar} style={{marginTop: '10px'}}>Incrementar</button>
+            <button className="counter" onClick={Decrementar} style={{ marginLeft: '10px' }}>Decrementar</button>
 
             <h2 className="title2">Histórico</h2>
             <ul>
